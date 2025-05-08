0c436bb1d8025ca7a893b3329bef3765b9cc63fb
fuadraihan10 <contact.fuadraihan@gmail.com>
2025-05-08 20:47:51 +0600

    adding modal

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.getElementById("history").addEventListener('click', function() {
     document.getElementById("donation").classList.add("btn-outline");
 
     document.getElementById("donation-section").classList.add("hidden");
+
+    document.getElementById("history-section").classList.remove("hidden");
   });
 
 // donation-sec----------------------
@@ -12,12 +14,15 @@ document.getElementById("donation").addEventListener('click', function() {
     document.getElementById("history").classList.add("btn-outline");
 
     document.getElementById("donation-section").classList.remove("hidden");
+
+    document.getElementById("history-section").classList.add("hidden");
   });
 
 
 // --donate-button---
 function donate(input,button,coin) {
 
+  // coin calculation-----------------------
   document.getElementById(button).addEventListener("click", function() {
     var capital = document.getElementById("capital").innerText;
     var amount = document.getElementById(input).value;
@@ -28,7 +33,9 @@ function donate(input,button,coin) {
     }
     document.getElementById("capital").innerText =  parseInt(capital) - parseInt(amount);
     document.getElementById(coin).innerText =  parseInt(coin_amount) + parseInt(amount);
+    my_modal_1.showModal();
 
+    
 
 });
 }
