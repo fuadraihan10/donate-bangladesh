57e4743b5a53b9bfb738b5aaf48a754a77d0f5c7
fuadraihan10 <contact.fuadraihan@gmail.com>
2025-05-08 16:38:00 +0600

    addding donation section

--- script.js
+++ script.js
@@ -2,10 +2,39 @@
 document.getElementById("history").addEventListener('click', function() {
     document.getElementById("history").classList.remove("btn-outline");
     document.getElementById("donation").classList.add("btn-outline");
+
+    document.getElementById("donation-section").classList.add("hidden");
   });
 
 // donation-sec----------------------
 document.getElementById("donation").addEventListener('click', function() {
     document.getElementById("donation").classList.remove("btn-outline");
     document.getElementById("history").classList.add("btn-outline");
+
+    document.getElementById("donation-section").classList.remove("hidden");
   });
+
+
+// --donate-button---
+function donate(input,button,coin) {
+
+  document.getElementById(button).addEventListener("click", function() {
+    var capital = document.getElementById("capital").innerText;
+    var amount = document.getElementById(input).value;
+    coin_amount = document.getElementById(coin).innerText;
+    if (amount === "") {
+      alert("Please enter an amount to donate.");
+      return;
+    }
+    document.getElementById("capital").innerText =  parseInt(capital) - parseInt(amount);
+    document.getElementById(coin).innerText =  parseInt(coin_amount) + parseInt(amount);
+
+
+});
+}
+
+// donate---------------------------
+donate("noakhali-input","noakhali-donate","noakhali-coin");
+donate("feni-input","feni-donate","feni-coin");
+donate("quata-input","quata-donate","quata-coin");
+
